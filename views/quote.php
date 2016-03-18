d181d06b90b70a86d80a39abb53c0dcedb19ec99
Daniela Sofía Villegas Ortega <danysofyvillegas@gmail.com>
2016-03-18 02:13:09 +0000

    change on quote.php, gliphycon added and tipography changed

--- views/quote.php
+++ views/quote.php
@@ -1,9 +1,7 @@
-<h1><?php echo $stock["name"]; ?></h1>
-<h2>$<?php echo number_format($stock["price"],2); ?></h2>
-<h2>Symbol: <?php echo $stock["symbol"]; ?></h2>
+<h1 style ="font-family: Verdana; color: #4373F4"><?php echo $stock["name"]; ?></h1>
+<h2 style ="font-family: Verdana; color: #2DAB7B">$<?php echo number_format($stock["price"],2); ?></h2>
+<h2 style ="font-family: Verdana; color: #8773DA">Symbol: <?php echo $stock["symbol"]; ?></h2>
 <!-- boton -->
 <div class = "form-group">
-    <a class = "btn btn-default" href= "buy.php?symbol=<?=$stock["symbol"]?>">
-    Buy
-    </a>
+    <a class = "btn btn-default" href= "buy.php?symbol=<?=$stock["symbol"]?>"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>Buy</a>
 </div>
