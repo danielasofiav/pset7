d5ac91bb5e50e3885537fa7b6e9fae46ada2fbe6
Daniela Sofía Villegas Ortega <danysofyvillegas@gmail.com>
2016-03-18 02:33:02 +0000

    image added at quote form

--- views/quote_form.php
+++ views/quote_form.php
@@ -10,5 +10,8 @@
                 Quote
             </button>
         </div>
+        <div>
+            <img src="http://yourfinancebook.com/wp-content/uploads/2015/11/shares-transmission.jpg" class="img-responsive" alt="Responsive image">
+        </div>
     </fieldset>
 </form>
